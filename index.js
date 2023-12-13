a06f6b0f3a9f3a763c017f699a1771e758e952aa
hoopesew <hoopesew@gmail.com>
2023-12-13 00:58:09 -0700

    about page stuff

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-let app = express(); //makes a new express application for the website
+const app = express(); //makes a new express application for the website
 let path = require('path'); //makes it easier to find files
 const port = process.env.PORT || 8080; //specifies the port to listen on
 app.set('view engine', 'ejs');
@@ -105,5 +105,8 @@ app.get('/signup', (req, res) => {
     //send a response indicating success
     res.send('Account created successfully!');
   });
+
+  //this is the route for images being displayed on the website 
+app.use(express.static('public'));
 
 app.listen(port, () => console.log('My server is listening'));
